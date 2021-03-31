c8d489e70d9dcdecdbcdb8a9af43e5eb1a19c728
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-31 17:54:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+// env_vars import
+require('dotenv/config');
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+// Usando o modo hawkins
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === false;
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Escutando na porta 3000');
+});
